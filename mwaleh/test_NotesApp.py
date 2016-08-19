f1cc91de885d1812176b43acb0e534b339343f8a
Martin Cornel <oumartinc@gmail.com>
2016-08-19 15:35:14 +0300

    Tests the NotesApp (still under development)

---
+++ mwaleh/test_NotesApp.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+import NotesApp
+
+class NotesAppTest (TestCase):
+	Note1 = 'This is the first note I save in my Notes application'
+	Note2 = 'The moment I tried Python programming, I realized it wa the best thing that ever was.'
+	Note3 = 'Cultivate the habit of speaking well of others'
+	Note4 = 'Dwell upon the good qualities of those with whom you associate, and see as little as possible of their errors and failings.'
+	Note5 = 'When tempted to complain of what some one has said or done, praise something in that person\'s life or character. Cultivate thankfulness.'
+	Note6 = 'raise God for His wonderful love in giving Christ to die for us. It never pays to think of our grievances.'
+	Note7 = '5 + 7'
+	Note8 = '[this is a list]'
+
+	def TestCreate(self):
+		''' Tests the create funcion of the NotesApp '''
+		pass
+
+	def TestList(self):
+		'''Tests the List function of the NotesApp '''
+		pass
+
+	def TestGet(self):
+		'''Tests the 
+
